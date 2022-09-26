78f94b58865812238490b863f16e5c32d052c71e
pouriya chibaie <pouriya.chibaie.dev@gmail.com>
2022-09-26 15:47:21 +0330

    add query update insert delete getperID

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const mysql=require("mysql2");
 const bodyParser=require('body-parser');
 const express = require('express');
-const { json } = require("body-parser");
-const { JSON } = require("sequelize");
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
  
@@ -18,39 +17,77 @@ con.connect((err) => {
   });
 
 app.get('/', (req, res) => {
-    res.send('<center><h1>Welcome to Edurekas REST API with Node.js Tutorial!!</h1></center> ');
+    res.status(200).send('<center><h1>Welcome to Edurekas REST API with Node.js Tutorial!!</h1></center> ');
 });
-app.get('/api/getStore', (req, res) => {
-   const data= con.query('SELECT * FROM stores', (err,rows) => {
-            if(err) throw err;
-            console.log('Data received from Db:');
- res.send(rows)
-})
-})
-app.get('/api/getStore/floor/:floor', (req, res) => {
-   const data= con.query(`SELECT * FROM stores where floor = ${req.params.floor}`, (err,rows) => {
-            if(err) throw err;
-            console.log('Data received from Db:');
- res.send(rows)
-})
-}) 
-app.get('/api/number-of-shops', (req, res) => {
-   const data= con.query(`SELECT COUNT(store_name) as numberOfStore FROM stores`, (err,rows) => {
-            if(err) throw err;
-            console.log('Data received from Db:');
- res.send(rows)
-})
-})
-app.post('/api/add-store', (req, res) => {
-   const data= con.query(`INSERT INTO stores (category_id, store_name, phoneNumber, instagram_id, store_number, floor)
-   VALUES (${parseInt (req.params.cid)}, ${req.params.storeName},${req.params.phoneNumber}, ${req.params.inId},${parseInt(req.params.storeNumber)}, ${parseInt(req.params.floor)});`, (err,rows) => {
-            if(err) throw err;
-            console.log('Data received from Db:');
- res.send(rows)
-})
-})
 
+
+app.get('/api/getStore', (req, res) => {
+    con.query('SELECT * FROM stores', (err,rows) => {
+            if(err) throw err;
+ res.status(200).send(rows)
+})})
+
+app.get('/api/get-store/floor/:floor', (req, res) => {
+    con.query(`SELECT * FROM stores where floor = ${req.params.floor}`, (err,rows) => {
+            if(err) throw err;
+ res.status(200).send(rows)
+})}) 
+app.get('/api/get-store/:id', (req, res) => {
+    con.query(`SELECT * FROM stores where id = ${req.params.id}`, (err,rows) => {
+            if(err) throw err;
+ res.status(200).send(rows)
+})}) 
+app.get('/api/search-store/:name', (req, res) => {
+    con.query(`select * from stores where store_name like '%${req.params.name}%'`, (err,rows) => {
+            if(err) throw err;
+ res.status(200).send(rows)
+})}) 
+app.get('/api/delete-store/:id', (req, res) => {
+    con.query(`delete from stores where id = ${req.params.id}`, (err,rows) => {
+            if(err) throw err; 
+ res.status(200).send(rows)
+})}) 
+app.post('/api/update-store/:id', (req, res) => {
+    con.query(`UPDATE stores
+    SET category_id = ?,store_name=? , phoneNumber=?,instagram_id=? ,store_number=?,floor=?
+    WHERE id = ?; `,[
+      parseInt( req.query.cid),
+      req.query.storeName,     
+      req.query.phoneNumber,
+      req.query.inId,
+       parseInt( req.query.storeNumber),
+      parseInt(  req.query.floor),
+   req.params.id]
+    , (err,rows) => {
+            if(err) throw err;
+ res.status(200).send(rows)
+})}) 
+
+
+app.get('/api/number-of-shops', (req, res) => {
+    con.query(`SELECT COUNT(store_name) as numberOfStore FROM stores`, (err,rows) => {
+            if(err) throw err;
+ res.status(200).send(rows)
+})})
+
+
+
+app.post('/api/add-store', (req, res) => {
+
+const sql = `INSERT INTO stores (category_id, store_name, phoneNumber, instagram_id, store_number, floor) VALUES ?`
+const values = [[
+ parseInt( req.query.cid),
+  req.query.storeName,     
+  req.query.phoneNumber,
+  req.query.inId,
+ parseInt( req.query.storeNumber),
+parseInt(  req.query.floor)]]
+
+con.query(sql,[values],function(err,result){
+if(err) throw err;
+res.status(201).send({isSuccess:true})
+})})
 
 
 const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Listening on port ${port}..`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
